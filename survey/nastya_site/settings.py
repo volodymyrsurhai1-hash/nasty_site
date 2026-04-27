e553715b692a4a66eab694340245d35a42b44581
volodymyrsurhai <gamedeverun@gmail.com>
2026-04-27 14:50:01 +0300

    feat: initialize Django project with custom settings and dependency requirements

--- survey/nastya_site/settings.py
+++ survey/nastya_site/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 JAZZMIN_SETTINGS = {
